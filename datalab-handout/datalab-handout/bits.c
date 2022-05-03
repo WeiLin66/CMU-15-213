26b5bf35c1bf4c9be3488a9208f4d18d11e7a13b
Zach Lin <linhoway@gmail.com>
2022-05-03 23:50:42 +0800

    [Modify]update 1. datalab

--- datalab-handout/datalab-handout/bits.c
+++ datalab-handout/datalab-handout/bits.c
@@ -217,7 +217,16 @@ int conditional(int x, int y, int z) {
  *   Rating: 3
  */
 int isLessOrEqual(int x, int y) {
-  return !((0x01 << 31) ^ x) | (!(((y + (~x + 0x01))) >> 31)) | (!(x ^ y));
+  int a = (x >> 31) & 0x1; // check +/-
+  int b = (y >> 31) & 0x1; // check +/-
+
+  int c1 = (a & (!b)); // positive overflow
+  int c2 = ((!a) & b); // negative overflow
+
+  int e = y + ((~x) + 1);
+  int flag = e >> 31; // normal minus operation
+
+return c1 | ((!c2) & (!flag));
 }
 //4
 /* 
@@ -229,7 +238,7 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+  return ((((x ^ (~(0x01 << 31))) + 0x01) >> 31) & 0x01) & (((x ^ (0x01 << 31)) >> 31) & 0x01);
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
@@ -244,7 +253,21 @@ int logicalNeg(int x) {
  *  Rating: 4
  */
 int howManyBits(int x) {
-  return 0;
+    int b16, b8, b4, b2, b1, b0;
+    int flag = x >> 31;
+    x = (~flag & x) | (flag & ~x);
+    b16 = !!(x >> 16) << 4;
+    x >>= b16;
+    b8 = !!(x >> 8) << 3;
+    x >>= b8;
+    b4 = !!(x >> 4) << 2;
+    x >>= b4;
+    b2 = !!(x >> 2) << 1;
+    x >>= b2;
+    b1 = !!(x >> 1);
+    x >>= b1;
+    b0 = x;
+  return b0 + b1 + b2 + b4 + b8 + b16 +1;
 }
 //float
 /* 
