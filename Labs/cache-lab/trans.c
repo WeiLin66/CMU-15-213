6cdf79eed9146c681d8926479e4cecd89b3c2a34
Zach Lin <linhoway@gmail.com>
2022-10-24 22:42:15 +0800

    [Update] cache-lab 64*64

--- Labs/cache-lab/trans.c
+++ Labs/cache-lab/trans.c
@@ -23,32 +23,74 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N]){
 
-    int i, j, i1;
+    int i, j, k;
+    int temp_0, temp_1, temp_2, temp_3, temp_4, temp_5, temp_6, temp_7;
 
-    for (i = 0; i < M; i += BLOCK) {
+    for (i = 0; i < N; i += BLOCK) {
 
-        for (j = 0; j < N; j += BLOCK) {
-            
-            /* B * B mini block */
-            for(i1=i; i1<i+BLOCK; i1++) {
+        for (j = 0; j < M; j += BLOCK) {
 
-                int temp_0 = A[i1][j];
-                int temp_1 = A[i1][j+1];
-                int temp_2 = A[i1][j+2];
-                int temp_3 = A[i1][j+3];
-                int temp_4 = A[i1][j+4];
-                int temp_5 = A[i1][j+5];
-                int temp_6 = A[i1][j+6];
-                int temp_7 = A[i1][j+7];
 
-                B[j][i1] = temp_0;
-                B[j+1][i1] = temp_1;
-                B[j+2][i1] = temp_2;
-                B[j+3][i1] = temp_3;
-                B[j+4][i1] = temp_4;
-                B[j+5][i1] = temp_5;
-                B[j+6][i1] = temp_6;
-                B[j+7][i1] = temp_7;
+            /* A1, A2 --> B1, B2 */
+            for (k = 0; k < BLOCK/2; k++) {
+
+                temp_0 = A[i + k][j];
+                temp_1 = A[i + k][j + 1];
+                temp_2 = A[i + k][j + 2];
+                temp_3 = A[i + k][j + 3];
+                temp_4 = A[i + k][j + 4];
+                temp_5 = A[i + k][j + 5];
+                temp_6 = A[i + k][j + 6];
+                temp_7 = A[i + k][j + 7];
+
+                B[j][i + k] = temp_0;
+                B[j + 1][i + k] = temp_1;
+                B[j + 2][i + k] = temp_2;
+                B[j + 3][i + k] = temp_3;
+
+                B[j][i + k + 4] = temp_4;
+                B[j + 1][i + k + 4] = temp_5;
+                B[j + 2][i + k + 4] = temp_6;
+                B[j + 3][i + k + 4] = temp_7;
+            }
+
+            /* B2 --> B3, A3 --> B2 */
+            for (k = 0; k < BLOCK/2; k++) {
+
+                temp_0 = A[i + 4][j + k];
+                temp_1 = A[i + 5][j + k];
+                temp_2 = A[i + 6][j + k];
+                temp_3 = A[i + 7][j + k];
+
+                temp_4 = B[j + k][i + 4];
+                temp_5 = B[j + k][i + 5];
+                temp_6 = B[j + k][i + 6];
+                temp_7 = B[j + k][i + 7];
+
+                B[j + k][i + 4] = temp_0;
+                B[j + k][i + 5] = temp_1;
+                B[j + k][i + 6] = temp_2;
+                B[j + k][i + 7] = temp_3;
+
+                B[j + k + 4][i] = temp_4;
+                B[j + k + 4][i + 1] = temp_5;
+                B[j + k + 4][i + 2] = temp_6;
+                B[j + k + 4][i + 3] = temp_7;
+
+            }
+
+            /* A4 --> B4 */
+            for (k = 4; k < BLOCK; k++) {
+
+                temp_0 = A[i + k][j + 4];
+                temp_1 = A[i + k][j + 5];
+                temp_2 = A[i + k][j + 6];
+                temp_3 = A[i + k][j + 7];
+
+                B[j + 4][i + k] = temp_0;
+                B[j + 5][i + k] = temp_1;
+                B[j + 6][i + k] = temp_2;
+                B[j + 7][i + k] = temp_3;
             }
         }
     }
@@ -82,7 +124,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 char trans_desc32[] = "Simple transpose func32";
 void trans32(int M, int N, int A[N][M], int B[M][N]){ 
 
-    int i, j, i1, temp[BLOCK];
+    int i, j, i1;
 
     for (i = 0; i < M; i += BLOCK) {
 
@@ -91,23 +133,23 @@ void trans32(int M, int N, int A[N][M], int B[M][N]){
             /* B * B mini block */
             for(i1=i; i1<i+BLOCK; i1++) {
 
-                temp[0] = A[i1][j];
-                temp[1] = A[i1][j+1];
-                temp[2] = A[i1][j+2];
-                temp[3] = A[i1][j+3];
-                temp[4] = A[i1][j+4];
-                temp[5] = A[i1][j+5];
-                temp[6] = A[i1][j+6];
-                temp[7] = A[i1][j+7];
+                int temp_0 = A[i1][j];
+                int temp_1 = A[i1][j+1];
+                int temp_2 = A[i1][j+2];
+                int temp_3 = A[i1][j+3];
+                int temp_4 = A[i1][j+4];
+                int temp_5 = A[i1][j+5];
+                int temp_6 = A[i1][j+6];
+                int temp_7 = A[i1][j+7];
 
-                B[j][i1] = temp[0];
-                B[j+1][i1] = temp[1];
-                B[j+2][i1] = temp[2];
-                B[j+3][i1] = temp[3];
-                B[j+4][i1] = temp[4];
-                B[j+5][i1] = temp[5];
-                B[j+6][i1] = temp[6];
-                B[j+7][i1] = temp[7];
+                B[j][i1] = temp_0;
+                B[j+1][i1] = temp_1;
+                B[j+2][i1] = temp_2;
+                B[j+3][i1] = temp_3;
+                B[j+4][i1] = temp_4;
+                B[j+5][i1] = temp_5;
+                B[j+6][i1] = temp_6;
+                B[j+7][i1] = temp_7;
             }
         }
     }
@@ -129,7 +171,7 @@ void registerFunctions()
     /* Register any additional transpose functions */
     registerTransFunction(trans, trans_desc); 
 
-    registerTransFunction(trans32, trans_desc32);
+    // registerTransFunction(trans32, trans_desc32);
 
 }
 
